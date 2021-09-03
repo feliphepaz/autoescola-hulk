99b93568f49005cb4a4a9c663221d4f10169533f
feliphepaz <feliphe.s.paz@gmail.com>
2021-09-03 16:59:04 -0300

    menu mobile feito

---
+++ js/mobile.js
@@ -0,0 +1,16 @@
+export default function mobile() {
+  const btn = document.querySelector('.btn-mobile');
+  const btnImg = document.querySelector('.btn-mobile img');
+  const menu = document.querySelector('.header .container nav');
+
+  btn.addEventListener('click', handleMobile);
+
+  function handleMobile() {
+    menu.classList.toggle('active-mobile');
+    if (menu.classList.contains('active-mobile')) {
+      btnImg.src = '/img/close.svg';
+    } else {
+      btnImg.src = '/img/open.svg';
+    }
+  }
+}
--- script.js
+++ script.js
@@ -2,6 +2,11 @@ import slide from "./js/slide.js";
 import api from "./js/api.js";
 import handleFaq from "./js/handle-faq.js";
 import func from "./js/func.js";
+import mobile from "./js/mobile.js";
+import anima from "./js/anima.js";
+
+mobile();
+anima();
 
 if (document.body.classList.contains("home")) {
   slide();
